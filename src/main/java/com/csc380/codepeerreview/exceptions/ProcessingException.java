6a00d3ca5402a97f3c369331124977980a27aa08
Kevin Morales <kevinmorales@Kevins-MBP.lan>
2021-11-08 20:37:34 -0500

    Removed custom exceptions

--- src/main/java/com/csc380/codepeerreview/exceptions/ProcessingException.java
+++
@@ -1,7 +0,0 @@
-package com.csc380.codepeerreview.exceptions;
-
-public class ProcessingException extends Exception {
-    public ProcessingException(String message) {
-        super(message);
-    }
-}
