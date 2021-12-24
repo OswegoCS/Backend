2ca8d69ec9e4296644206d98471eba00c43136cb
Kevin Morales <kevinmorales@Kevins-MBP.lan>
2021-12-24 10:45:35 -0500

    Created course POJO

---
+++ src/main/java/com/csc380/codepeerreview/models/Course.java
@@ -0,0 +1,11 @@
+package com.csc380.codepeerreview.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class Course {
+    private int id;
+    private String name;
+}
