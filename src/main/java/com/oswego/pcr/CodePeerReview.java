4b98af7fa9ec09a9a2a50c799e4228090823fc5d
Kevin Morales <kevinm2052@gmail.com>
2022-01-13 17:29:51 -0500

    Added CORS restrictions to project

--- src/main/java/com/oswego/pcr/CodePeerReview.java
+++ src/main/java/com/oswego/pcr/CodePeerReview.java
@@ -2,12 +2,26 @@ package com.oswego.pcr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CodePeerReview {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodePeerReview.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CodePeerReview.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:80",
+                        "http://moxie.cs.oswego.edu:80");
+            }
+        };
+    }
 
 }
