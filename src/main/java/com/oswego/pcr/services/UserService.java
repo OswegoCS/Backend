2b12494befd37430be949ced86f7548519d30838
Kevin Morales <kevinm2052@gmail.com>
2022-01-15 19:36:03 -0500

    Removed unused method in user service

--- src/main/java/com/oswego/pcr/services/UserService.java
+++ src/main/java/com/oswego/pcr/services/UserService.java
@@ -1,12 +1,9 @@
 package com.oswego.pcr.services;
 
 import java.io.File;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import javax.annotation.Resource;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.oswego.pcr.models.Post;
 import com.oswego.pcr.models.User;
 import com.oswego.pcr.repositories.dao.PostDao;
@@ -14,7 +11,6 @@ import com.oswego.pcr.repositories.dao.UserDao;
 import com.oswego.pcr.responses.GetProfileResponse;
 import com.oswego.pcr.util.FileHelper;
 import com.oswego.pcr.util.RoleHelper;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,13 +21,11 @@ public class UserService {
     private final UserDao userRepo;
     @Resource
     private final PostDao postRepo;
-    private final ObjectMapper mapper;
 
     @Autowired
-    public UserService(UserDao userRepo, PostDao postRepo, ObjectMapper mapper) {
+    public UserService(UserDao userRepo, PostDao postRepo) {
         this.userRepo = userRepo;
         this.postRepo = postRepo;
-        this.mapper = mapper;
     }
 
     // Returns a profile of a user
@@ -50,14 +44,6 @@ public class UserService {
         response.setUser(user);
         response.setPosts(posts);
         return response;
-    }
-
-    public ObjectNode validateUser(String email) {
-        String decodedEmail = URLDecoder.decode(email, StandardCharsets.UTF_8);
-        boolean validity = userRepo.findByEmail(decodedEmail) != null ? true : false;
-        ObjectNode objectNode = mapper.createObjectNode();
-        objectNode.put("validity", validity);
-        return objectNode;
     }
 
     public void createStudents(File studentCSV) {
