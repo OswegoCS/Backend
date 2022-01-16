95c919f721960812ba1e6c505a0de0dd4961c9ce
Kevin Morales <kevinm2052@gmail.com>
2022-01-15 19:32:17 -0500

    Added method to convert objects to string for logging purposes

--- src/main/java/com/oswego/pcr/util/ObjectHelper.java
+++ src/main/java/com/oswego/pcr/util/ObjectHelper.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ObjectHelper {
@@ -15,9 +16,17 @@ public class ObjectHelper {
     public static <T> T convertStringToObject(String json, Class<T> objectClass) {
         try {
             return mapper.readValue(json, objectClass);
-        } catch (Exception e) {
+        } catch (JsonProcessingException e) {
             logger.error("Error parsing JSON", e.getMessage());
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error parsing JSON");
+        }
+    }
+
+    public static String convertObjectToString(Object obj) {
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error reading object");
         }
     }
 
